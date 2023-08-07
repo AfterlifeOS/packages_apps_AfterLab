3f1d23b3d8a469a2be8393cb29994a03de5bd715
Mr- 0 <oldcrocs21@gmail.com>
2023-08-07 13:29:30 +0000

    Redesign AfterLabs Welcome To Our Labs,..
    Credit:
    + AfterLife Team
    + Project radiant For Blur WallpaperView
    
    Signed-off-by: Mr- 0 <oldcrocs21@gmail.com>

--- src/com/afterlife/afterlab/CustomizeDashboard.java
+++ src/com/afterlife/afterlab/CustomizeDashboard.java
@@ -18,10 +18,14 @@ package com.afterlife.afterlab;
 import android.content.Context;
 import android.os.Bundle;
 import android.provider.SearchIndexableResource;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
@@ -47,6 +51,28 @@ public class CustomizeDashboard extends SettingsPreferenceFragment {
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.AFTERLIFE;
     }
+
+    @Override
+    public RecyclerView onCreateRecyclerView(LayoutInflater inflater, ViewGroup parent,
+            Bundle savedInstanceState) {
+        RecyclerView recyclerView = super.onCreateRecyclerView(inflater, parent,
+                savedInstanceState);
+        GridLayoutManager layoutManager = new GridLayoutManager(getActivity(), 2);
+		layoutManager.setSpanSizeLookup(new AfterlifeSpanSizeOP());
+		recyclerView.setLayoutManager(layoutManager);
+        return recyclerView;
+    }
+
+    class AfterlifeSpanSizeOP extends GridLayoutManager.SpanSizeLookup {
+		@Override
+		public int getSpanSize(int position) {
+		    if (position == 2 || position == 3 || position == 4 || position == 5 || position == 6 || position == 7 || position == 8 || position == 9 || position == 10) {
+				return 1;
+			} else {
+				return 2;
+			}
+		}
+	}
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
             new BaseSearchIndexProvider() {
