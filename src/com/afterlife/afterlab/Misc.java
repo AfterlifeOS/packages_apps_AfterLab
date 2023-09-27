d713d85cf20df598604034ac0a65774a256de4d2
Mr- 0 <oldcrocs21@gmail.com>
2023-09-27 14:17:46 +0000

    AfterLabs: User toggle for GamesProp [2/2]
    Signed-off-by: Mr- 0 <oldcrocs21@gmail.com>

--- src/com/afterlife/afterlab/Misc.java
+++ src/com/afterlife/afterlab/Misc.java
@@ -48,8 +48,11 @@ import java.util.List;
 public class Misc extends SettingsPreferenceFragment 
             implements Preference.OnPreferenceChangeListener {
     
+    private static final String KEY_GAMES_SPOOF = "use_games_spoof";
     private static final String KEY_PHOTOS_SPOOF = "use_photos_spoof";
+    private static final String SYS_GAMES_SPOOF = "persist.sys.pixelprops.games";
     private static final String SYS_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
+    private SwitchPreference mGamesSpoof;
     private SwitchPreference mPhotosSpoof;
 
     @Override
@@ -60,14 +63,22 @@ public class Misc extends SettingsPreferenceFragment
         final Resources res = getResources();
 
         mPhotosSpoof = (SwitchPreference) prefScreen.findPreference(KEY_PHOTOS_SPOOF);
+        mGamesSpoof = (SwitchPreference) findPreference(KEY_GAMES_SPOOF);
+        mGamesSpoof.setChecked(SystemProperties.getBoolean(SYS_GAMES_SPOOF, false));
+        mGamesSpoof.setOnPreferenceChangeListener(this);
         mPhotosSpoof.setChecked(SystemProperties.getBoolean(SYS_PHOTOS_SPOOF, true));
         mPhotosSpoof.setOnPreferenceChangeListener(this);
     }
     
     @Override
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-        if (preference == mPhotosSpoof) {
-            boolean value = (Boolean) objValue;
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mGamesSpoof) {
+            boolean value = (Boolean) newValue;
+            SystemProperties.set(SYS_GAMES_SPOOF, value ? "true" : "false");
+            return true;
+        } else if (preference == mPhotosSpoof) {
+            boolean value = (Boolean) newValue;
             SystemProperties.set(SYS_PHOTOS_SPOOF, value ? "true" : "false");
             return true;
         }
