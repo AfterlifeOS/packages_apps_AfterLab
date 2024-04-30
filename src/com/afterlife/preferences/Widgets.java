0fedcc9720cc016c6dfedd5d45ee0eddf77bc336
minaripenguin <minaripenguin@users.noreply.github.com>
2024-04-30 13:15:04 +0800

    Lockscreen widget: Add sound engine widget
    Change-Id: If9ad4f5e6193e778ea24f8e8d3df1efdf106bb69

--- src/com/afterlife/preferences/Widgets.java
+++ src/com/afterlife/preferences/Widgets.java
@@ -174,6 +174,8 @@ public class Widgets extends RelativeLayout {
                 break;
             case "bt":
                 setUpWidgetResources(widgetView, R.drawable.ic_bt, R.string.bt);
+            case "sound_engine":
+                setUpWidgetResources(widgetView, R.drawable.ic_settings_sound, R.string.sound_engine_title);
                 break;
             default:
                 break;
