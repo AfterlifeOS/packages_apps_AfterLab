6f3ffd3a14ecb6436932460e653c6e6a04146b94
Mr_ZerO <rbesar210311@gmail.com>
2024-06-19 15:19:25 +0000

    AfterLabs: Bring back clock AM/PM style
    Change-Id: I6d3e0bb50a7b6387b9b44b85ea2b66ddb57bd2ea

--- src/com/afterlife/afterlab/fragments/StatusBarClock.java
+++ src/com/afterlife/afterlab/fragments/StatusBarClock.java
@@ -47,6 +47,8 @@
 
  import com.android.settingslib.search.Indexable;
  import com.android.settingslib.search.SearchIndexable;
+ 
+ import com.afterlife.support.preference.SecureSettingListPreference;
 
  import java.util.ArrayList;
  import java.util.Date;
@@ -57,19 +59,27 @@
          implements Preference.OnPreferenceChangeListener, Indexable {
 
      private static final String TAG = "StatusBarClock";
+     private static final String KEY_STATUS_BAR_AM_PM = "status_bar_am_pm";
+
+     private SecureSettingListPreference mStatusBarAmPm;
 
      @Override
      public void onCreate(Bundle savedInstanceState) {
          super.onCreate(savedInstanceState);
-         addPreferencesFromResource(R.xml.category_statusbar);
+         addPreferencesFromResource(R.xml.clock_date);
 
          final ContentResolver resolver = getActivity().getContentResolver();
          final PreferenceScreen screen = getPreferenceScreen();
+         mStatusBarAmPm = findPreference(KEY_STATUS_BAR_AM_PM);
      }
 
      @Override
      public void onResume() {
          super.onResume();
+         if (DateFormat.is24HourFormat(requireContext())) {
+         mStatusBarAmPm.setEnabled(false);
+         mStatusBarAmPm.setSummary(R.string.status_bar_am_pm_unavailable);
+         }
      }
 
      @Override
@@ -89,7 +99,7 @@
                          boolean enabled) {
                      final ArrayList<SearchIndexableResource> result = new ArrayList<>();
                      final SearchIndexableResource sir = new SearchIndexableResource(context);
-                     sir.xmlResId = R.xml.category_statusbar;
+                     sir.xmlResId = R.xml.clock_date;
                      result.add(sir);
                      return result;
                  }
